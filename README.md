99e419eef101d433587613ad57883247550190df
Bess Campbell <bess.k.campbell@gmail.com>
2020-12-10 11:10:07 -0800

    show code for passing constructor test
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+
+// Notes
+ convert word 
+// apple -> {"a", "p"}
+
+
+
+// list = [
+//   "a" : 1
+//   "b" : 3
+//   "c" : 4
+// ]
+
+// score;
+// for i in range(Word) {
+//   score = score + word[i]
+//   list["a"] = 1
+// }
---
+++ Scrabble/Models/Word.cs
@@ -0,0 +1,16 @@
+
+
+namespace Scrabble.Models
+{
+  public class Word
+  {
+    public string UserInput { get; }
+    public int Score { get; }
+    public Word(string userInput)
+    {
+      UserInput = userInput;
+      Score = 0;
+    }
+    
+  }
+}
